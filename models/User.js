a6feddb4190797688939d1825333d5d9216ff859
Zaid Nissar <zupanditzaid@gmail.com>
2024-11-09 22:35:12 -0700

    Mongo Support

--- models/User.js
+++ models/User.js
@@ -1,14 +1,24 @@
 // models/User.js
 import mongoose from 'mongoose';
-import { type } from 'os';
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
-},
-  email: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  userBio: {
+    type: {
+      country: String,
+      inCanada: Boolean,
+      whySeekAsylum: String,
+    },
+    required: false, // Adjust this based on your requirements
+  },
 });
 
-// Check if the model is already compiled to avoid re-compilation issues in development
-export default mongoose.models.User || mongoose.model('User', UserSchema);
+// Avoid recompiling the model in development
+export default mongoose.model('User', UserSchema);
